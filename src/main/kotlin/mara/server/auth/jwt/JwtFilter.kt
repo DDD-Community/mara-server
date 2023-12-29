c99249eee0e03be952cc54967e9f9593ce408f90
Jiwon-cho <gwon188@gmail.com>
2023-12-29 13:16:56 +0900

    feat: jwt 코드 추가

---
+++ src/main/kotlin/mara/server/auth/jwt/JwtFilter.kt
@@ -0,0 +1,31 @@
+package com.rainbow.server.auth.jwt
+
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class JwtFilter(private val jwtProvider: JwtProvider) : OncePerRequestFilter() {
+
+    private fun HttpServletRequest.getToken(): String? {
+        val rawToken = this.getHeader("Authorization")
+        return if (rawToken != null && rawToken.startsWith("Bearer"))
+            rawToken.replace("Bearer ", "")
+        else null
+    }
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain,
+    ) {
+        val jwt = request.getToken()
+
+        if (jwt != null && jwtProvider.validate(jwt)) {
+            SecurityContextHolder.getContext().authentication =
+                jwtProvider.getAuthentication(jwt)
+        }
+        filterChain.doFilter(request, response)
+    }
+}
